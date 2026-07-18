6a0632940d60c73f69fa6a52a8bb299b3abbebe7
TheAM01 <onevisionofficial55@gmail.com>
2026-07-18 19:40:40 +0500

    Updated the products page to fetch from API

--- app/(store)/products/page.tsx
+++ app/(store)/products/page.tsx
@@ -15,10 +15,10 @@ export default async function AllProductsPage({ searchParams }: { searchParams: 
     const { order, sort, brand, q } = await searchParams;
 
 
-    const res = await fetch("http://localhost:3000/api/products", {
+    const res = await fetch("https://218-next.vercel.app/api/products", {
         method: "GET"
     });
-    const products: Product[] = await res.json();
+    const {data: products}: { data: Product[] } = await res.json();
     let filteredProducts = products;
 
     if (q) {
