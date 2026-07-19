e08baca33e6fe2c468a6ae2e838f6c0e8b088313
TheAM01 <onevisionofficial55@gmail.com>
2026-07-19 20:21:48 +0500

    Fixed lib/products import

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,10 +1,10 @@
 import { Product } from "@/types/product";
 import ProductDashboardClient from "./ProductDashboardClient";
-import { products } from "@/lib/products";
+// import { products } from "@/lib/products";
 
 export default async function ProductDashboardPage() {
-    // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
-    // const {data: products}: { data: Product[] } = await res.json();
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
+    const {data: products}: { data: Product[] } = await res.json();
 
     return <ProductDashboardClient products={products} />
 }
